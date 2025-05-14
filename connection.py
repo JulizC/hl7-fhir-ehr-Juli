decd6f797dc2120fd828b02966cd7da1b372a8e5
JulizC <juliana.cabrejo-m@mail.escuelaing.edu.co>
2025-05-14 18:44:08 -0500

    cambios uri

--- connection.py
+++ connection.py
@@ -1,10 +1,14 @@
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
-
+import urllib.parse
 
 def connect_to_mongodb(db_name, collection_name):
-    uri = "mongodb+srv://julianacabrejo:Julienzo@ifmerjuli.7ev8g.mongodb.net/?retryWrites=true&w=majority&appName=ifmerJuli"
+    user = urllib.parse.quote_plus("juliana.cabrejo@urosario.edu.co")
+    password = urllib.parse.quote_plus("Julienzo")
+    uri = f"mongodb+srv://juliana.cabrejo:Mongo1234@ifmerjuli.7ev8g.mongodb.net/?retryWrites=true&w=majority&appName=ifmerJuli"
+    
     client = MongoClient(uri, server_api=ServerApi('1'))
     db = client[db_name]
     collection = db[collection_name]
     return collection
+
